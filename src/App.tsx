711d6c4def730c2b163ff24c9824ab4cc83a4ee4
Gabriel Schrock <gabriel.schrock@wolflair.com>
2019-05-04 14:37:01 -0500

    bootstrap and ability score, class options, and class prime req utils (#2)
    * initial app setup, includes node roll, lodash, and styled components, and initial character data
    
    * adds utils file with empty equipment util file
    
    * adds abilityscore, class options, class prime req utils, very basic button class and prime req display

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { sampleSize } from "lodash";
 import React from "react";
-import Roll from "roll";
+import Button from "react-bootstrap/Button";
 import styled from "styled-components";
 import { gear } from "./characterData/gear";
+import { getAbilityScores } from "./utils/getAbilityScores";
+import { getClassOptionsToDisplay } from "./utils/getClassOptionsToDisplay";
+import { getClassPrimeRequisites } from "./utils/getClassPrimeRequisites";
 
 interface Props {
   className?: string;
@@ -11,28 +14,86 @@ interface Props {
 interface ImplProps extends Props {}
 
 const AppImpl: React.SFC<ImplProps> = ({ className }) => {
-  const roller = new Roll();
-  const str = roller.roll("3d6").result;
-  const dex = roller.roll("3d6").result;
-  const con = roller.roll("3d6").result;
-  const int = roller.roll("3d6").result;
-  const wis = roller.roll("3d6").result;
-  const cha = roller.roll("3d6").result;
+  const abilityScores = getAbilityScores();
+  const classOptions = getClassOptionsToDisplay(abilityScores);
   return (
     <div className={className}>
-      <div>{str}</div>
-      <div>{dex}</div>
-      <div>{con}</div>
-      <div>{int}</div>
-      <div>{wis}</div>
-      <div>{cha}</div>
+      <AbilityScoresContainer>
+        <AbilityScore>STR</AbilityScore>
+        <AbilityScore>DEX</AbilityScore>
+        <AbilityScore>CON</AbilityScore>
+        <AbilityScore>INT</AbilityScore>
+        <AbilityScore>WIS</AbilityScore>
+        <AbilityScore>CHA</AbilityScore>
+      </AbilityScoresContainer>
+      <AbilityScoresContainer>
+        {abilityScores.map((abilityScore, index) => (
+          <AbilityScore key={index}>{abilityScore}</AbilityScore>
+        ))}
+      </AbilityScoresContainer>
+      <ClassButtonsContainer>
+        {/**
+        @todo
+        check class options here for any demihumans, if any
+        are in class options include note about how dorky
+        b/x treats races as classes (make a util)
+        */}
+        {classOptions.map((classOption: string) => (
+          <ClassOptionContainer key={classOption}>
+            <ButtonContainer>
+              <ClassButton variant="outline-secondary">
+                {classOption}
+              </ClassButton>
+            </ButtonContainer>
+            <ClassPrimeRequisites>
+              {getClassPrimeRequisites(classOption)}
+            </ClassPrimeRequisites>
+          </ClassOptionContainer>
+        ))}
+      </ClassButtonsContainer>
       <div>{`Two random items: ${sampleSize(gear, 2)}`}</div>
     </div>
   );
 };
 
+const AbilityScore = styled.div`
+  color: white;
+  font-size: 1rem;
+  background-color: black;
+  border-right: 1px solid white;
+  border-left: 1px solid white;
+  padding: 0.5rem;
+  width: 3rem;
+  text-align: center;
+`;
+
+const AbilityScoresContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ClassButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3rem;
+`;
+
+const ButtonContainer = styled.div`
+  padding: 0.75rem;
+`;
+
+const ClassOptionContainer = styled.div``;
+
+const ClassButton = styled(Button)``;
+
+const ClassPrimeRequisites = styled.div``;
+
 const StyledApp = styled(AppImpl)`
-  color: red;
+  .btn {
+    width: 100%;
+  }
+  /* display: flex;
+  justify-content: center; */
 `;
 
 export default StyledApp;
---
+++ src/constants/abilityScoreConstants.ts
@@ -0,0 +1,6 @@
+export const STR = 0;
+export const DEX = 1;
+export const CON = 2;
+export const INT = 3;
+export const WIS = 4;
+export const CHA = 5;
