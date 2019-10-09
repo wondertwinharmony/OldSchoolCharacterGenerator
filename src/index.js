9a5038fc0687345cd836f99a9b60bbe884c863a9
amenotu <kyabut@gmail.com>
2019-10-08 17:51:22 -0700

    Using AppRouter to enable front-end routing and navigation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCaretDown, faCaretUp, faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.tsx";
+import AppRouter from "./AppRouter";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 library.add(faCaretUp, faCaretDown, faExclamationTriangle);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
