dc3aa64f4044ef5390db0dfcef4ad91e31a028ab
Kristine Yabut <amenotu@users.noreply.github.com>
2019-10-22 11:32:00 -0700

    New Features (#28)
    * Saves modifications to package-lock.json and tsconfig.json
    
    * Updates package-lock.json
    
    * Using AppRouter to enable front-end routing and navigation
    
    * Added react-router, react-router-dom and associated types
    
    * Removed character component and enabled the use of browser history with react-router-dom
    
    * Added toCamelCase function
    
    * Updated getSavedCharacterData to handle route path
    
    * (feat) Added front-end routing and navigation with React-Router
    
    * Changed file type from tsx to ts
    
    * (feat) knave spells option now persists when character is generated
    
    * Fixed formatting issues with spells during permalink
    
    * (feat) Add reroll ability scores and handling for previously saved character URL
    
    * Added rolling dice icon
    
    * Code clean up
    
    * Update AppRouter.tsx
    
    Remove comment

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -8,26 +8,45 @@ import { getSavedCharacterData } from "./utils/getSavedCharacterData";
 
 
 export default function App() {
-    const [abilityScores] = useState(getAbilityScores);
+    const [abilityScores, setAbilityScores] = useState(getAbilityScores);
     const [isKnaveSpellsIncluded, setKnaveSpells] = useState(false);
 
     useEffect(() => {
       setKnaveSpells(isKnaveSpellsIncluded);
     }, [isKnaveSpellsIncluded]);
 
+    useEffect(() => {
+      setAbilityScores(abilityScores);
+    }, [abilityScores]);
+
     type TParams = { character: string,
-                     saved: string
+                     saved: string,
+                     previouslySaved: string
     
     }
 
+
     function CharacterSheet({ match }: RouteComponentProps<TParams>) {
+      const homeURL =
+      process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : "https://oldschoolknave.surge.sh/";
+      const baseURL = window.location.href;
+
+        if(match.params.previouslySaved){
+          let characterData = getSavedCharacterData(baseURL, homeURL, '');
+
+          return (
+            <StyledCreatedCharacter
+            classSelection={characterData && characterData.class}
+            abilityScores={characterData && characterData.abilityScores}
+            includeKnaveSpells={characterData.knave}
+            savedCharacterData={characterData}
+          />
+          )
+        }
 
         if(match.params.saved){
-            const homeURL =
-                process.env.NODE_ENV === "development"
-                ? "http://localhost:3000/"
-                : "https://oldschoolknave.surge.sh/";
-            let baseURL = window.location.href;
             let savedData = getSavedCharacterData(baseURL, homeURL, "savedCharacter/1&");
 
             return (
@@ -45,7 +64,7 @@ export default function App() {
         let abilityStringScoreArr = generatedCharacterData[1].split(',');
         let knave = generatedCharacterData[2] === 'true';
         
-        let hyphenatedCharacters = ['magic-user', 'crab-person', 'half-elf', 'wild magic-user'];
+        let hyphenatedCharacters = ['magic-user', 'crab-person', 'half-elf', 'wild magic-user', 'half-orc'];
 
         if(hyphenatedCharacters.includes(characterClass)){
            characterClass = toCamelCase(characterClass);
@@ -66,10 +85,14 @@ export default function App() {
               renders the first one that matches the current URL. */}
           <Switch>
             <Route exact path="/">
-              <StyledApp abilityScores={abilityScores} setKnaveSpells={setKnaveSpells} isKnaveSpellsIncluded={isKnaveSpellsIncluded}/>
+              <StyledApp abilityScores={abilityScores} 
+                         setKnaveSpells={setKnaveSpells} 
+                         isKnaveSpellsIncluded={isKnaveSpellsIncluded}
+                         setAbilityScores={setAbilityScores}/>
             </Route>
             <Route path="/savedCharacter/:saved/" component={CharacterSheet}></Route>
             <Route path="/generatedCharacter/:character/" component={CharacterSheet}></Route>
+            <Route path="/:previouslySaved/" component={CharacterSheet}></Route>
           </Switch>
       </Router>
     );
