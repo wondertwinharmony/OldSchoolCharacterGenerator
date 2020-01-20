dc8e8f1b1212b82655febf4bb3fc40626f906396
amenotu <kyabut@gmail.com>
2020-01-19 21:22:52 -0800

    Added context

---
+++ src/AppContext.ts
@@ -0,0 +1,22 @@
+import { createContext } from 'react';
+import { SavedCharacterData } from './utils/getSavedCharacterData';
+
+interface IAppContext {
+    abilityScores: number[];
+    classSelection: string;
+    includeKnaveSpells: boolean;
+    savedCharacterData?: SavedCharacterData;
+    setKnaveSpells: (isKnaveSpellsIncluded: boolean) => void;
+    setAbilityScores: (abilityScores: number[]) => void;
+  }
+
+const AppContext = createContext<IAppContext>({
+    abilityScores: [0,0,0,0,0,0],
+    classSelection: "fighter",
+    includeKnaveSpells: false,
+    savedCharacterData: undefined,
+    setKnaveSpells: () => {},
+    setAbilityScores: () => {}
+});
+
+export default AppContext;
