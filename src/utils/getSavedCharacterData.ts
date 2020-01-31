a1d504f267dc64485b7bf3e866cf54116ac21412
amenotu <kyabut@gmail.com>
2020-01-30 21:50:30 -0800

    Added url shortener feature

--- src/utils/getSavedCharacterData.ts
+++ src/utils/getSavedCharacterData.ts
@@ -1,13 +1,14 @@
 export interface SavedCharacterData {
-    name: string,
-    class: string,
-    traits: string,
-    abilityScores: number[],
-    hitPoints: number,
-    languages: string,
-    spells: string[],
-    equipment: { characterEquipmentString: string, slotsToFill: number }
-};
+  name: string;
+  class: string;
+  traits: string;
+  abilityScores: number[];
+  hitPoints: number;
+  languages: string;
+  spells: string[];
+  equipment: { characterEquipmentString: string; slotsToFill: number };
+  knave: boolean;
+}
 /**
  * Utility function that extract data from a URL.
  * Returns an object with character data needed to rehydrate a page.
@@ -15,41 +16,50 @@ export interface SavedCharacterData {
  * @param  {string} home
  * @param  {string} route
  */
-export const getSavedCharacterData = (url: string, home: string, route: string) => {
-    let decodedURI = '';
-    let rawCharacterData = [];
+export const getSavedCharacterData = (
+  url: string,
+  home: string,
+  route: string
+) => {
+  let decodedURI = "";
+  let rawCharacterData = [];
 
-    decodedURI = decodeURI(url).replace(home, '')
-                                   .replace(route, '')
-                                   .replace(/\$/gm, '&')
-                                   .replace(/-/gm, ' ')
+  decodedURI = decodeURI(url)
+    .replace(home, "")
+    .replace(route, "")
+    .replace(/\$/gm, "&")
+    .replace(/-/gm, " ");
 
-    rawCharacterData = decodedURI.split('&');
-    
-    let savedCharacterData = {
-        name: '',
-        class: '',
-        traits: '',
-        abilityScores: [] as number[],
-        hitPoints: 0,
-        languages: '',
-        spells: [] as string[],
-        equipment: { characterEquipmentString: '', slotsToFill: 0 },
-        knave: false
-    };
+  rawCharacterData = decodedURI.split("&");
 
-    let abilityScores = rawCharacterData[3].split(',');
+  let savedCharacterData = {
+    name: "",
+    class: "",
+    traits: "",
+    abilityScores: [] as number[],
+    hitPoints: 0,
+    languages: "",
+    spells: [] as string[],
+    equipment: { characterEquipmentString: "", slotsToFill: 0 },
+    knave: false
+  };
 
-    savedCharacterData.name = rawCharacterData[0].replace(/_/gm, ' ');
-    savedCharacterData.class = rawCharacterData[1];
-    savedCharacterData.traits = rawCharacterData[2].replace(/_/gm, ' ');
-    savedCharacterData.abilityScores = abilityScores.map(item => parseInt(item));
-    savedCharacterData.hitPoints = parseInt(rawCharacterData[4]);
-    savedCharacterData.languages = rawCharacterData[5].replace(/_/gm, ' ');
-    savedCharacterData.spells = rawCharacterData[6].replace(/_/gm, ' ').split('#');
-    savedCharacterData.equipment.characterEquipmentString = rawCharacterData[7].replace(/_/gm, ' ').replace('5’ 10’ / 11’ 20’ / 21’ 30’', '5’-10’ / 11’-20’ / 21’-30’');
-    savedCharacterData.equipment.slotsToFill = parseInt(rawCharacterData[8]);
-    savedCharacterData.knave = rawCharacterData[9] === 'true';
+  let abilityScores = rawCharacterData[3].split(",");
 
-    return savedCharacterData;
-}
+  savedCharacterData.name = rawCharacterData[0].replace(/_/gm, " ");
+  savedCharacterData.class = rawCharacterData[1];
+  savedCharacterData.traits = rawCharacterData[2].replace(/_/gm, " ");
+  savedCharacterData.abilityScores = abilityScores.map(item => parseInt(item));
+  savedCharacterData.hitPoints = parseInt(rawCharacterData[4]);
+  savedCharacterData.languages = rawCharacterData[5].replace(/_/gm, " ");
+  savedCharacterData.spells = rawCharacterData[6]
+    .replace(/_/gm, " ")
+    .split("#");
+  savedCharacterData.equipment.characterEquipmentString = rawCharacterData[7]
+    .replace(/_/gm, " ")
+    .replace("5’ 10’ / 11’ 20’ / 21’ 30’", "5’-10’ / 11’-20’ / 21’-30’");
+  savedCharacterData.equipment.slotsToFill = parseInt(rawCharacterData[8]);
+  savedCharacterData.knave = rawCharacterData[9] === "true";
+
+  return savedCharacterData;
+};
